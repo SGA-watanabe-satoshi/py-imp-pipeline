7bc034e3258852de090d35c76f25164eeb9d584a
satoshi <swatanabe@jp.sony.com>
2018-01-22 16:32:56 +0900

    update pipeline

--- bin/imp-pipeline.py
+++ bin/imp-pipeline.py
@@ -16,20 +16,46 @@ from apache_beam.io.gcp.internal.clients import bigquery
 
 logger = logging.getLogger(__name__)
 
-def getSchemea():
-    schema = bigquery.TableSchema()
+class BQColumnJsonStruct:
+    def __init__(self, tableFieldSchema, jsonPath):
+        self.__tableFieldSchema = tableFieldSchema
+        self.__jsonPath = jsonPath
 
-    field = bigquery.TableFieldSchema()
-    field.name = 'report_id'
-    field.type = 'string'
-    field.mode = 'required'
-    schema.fields.append(field)
+    def getFieldSchema(self):
+        return self.__tableFieldSchema
 
-    return schema
+    def getJsonPath(self):
+        return self.__jsonPath
 
-def parse_pubsub(line):
-    record = json.load(line)
-    return record
+class BQColumnJsonMap():
+    def __init__(self):
+        self.__dict = {}
+
+    def add(self, field_name, field_type, field_mode, jsonPath):
+        field = bigquery.TableFieldSchema()
+        field.name = field_name
+        field.type = field_type
+        field.mode = field_mode
+        self.__dict[field_name] = BQColumnJsonStruct(field, jsonPath)
+
+    def getSchema(self):
+        schema = bigquery.TableSchema()
+        field = bigquery.TableFieldSchema()
+        for v in self.__dict.values():
+            schema.fields.append(v.getFieldSchema())
+        return schema
+
+    def jsonToDict(self, line):
+        dic = {}
+        for k, v in self.__dict.items():
+            dic[k] = self.__getValue(json.loads(jsonStr), v.getJsonPath())
+        return dic
+
+    def __getValue(self, target, path):
+        value = target[path.pop(0)]
+        if len(path) > 0 and value is not None:
+            return self.getValue(value, path)
+        return value
 
 def run(argv=None):
 
@@ -44,22 +70,24 @@ def run(argv=None):
     pipeline_options.view_as(StandardOptions).streaming = True
 
     subscription = known_args.subscription
-
+ 
     logger.debug(pipeline_options)
 
     p = beam.Pipeline(options=pipeline_options)
 
+    placement_event = BQColumnJsonMap()
+    placement_event.add('report_id','string','required',['id'])
+
     ( p | 'Read from PubSub' >> beam.io.ReadStringsFromPubSub(subscription=subscription) | beam.WindowInto(window.FixedWindows(20))
-        | 'JSON to Dict' >> bema.Map(parse_pubsub),
+        | 'JSON to Dict' >> bema.Map(placement_event.jsonToDict),
         | 'Write to BigQuery' >> beam.io.WriteToBigQuery(
             known_args.output,
-            schema=getSchemea(),
+            schema=placement_event.getSchemea(),
             create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
             write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND)
     )
-
-    p.run()
     
+    p.run()
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.DEBUG)
